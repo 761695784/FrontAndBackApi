67664705688e3f2a7abbe055aabdb7f127b322b0
Malcom <malcom70976@gmail.com>
2024-08-03 00:50:46 +0000

    test avec Postmann en marche

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Article::all();
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+        return Article::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $article = Article::find($id);
+        if (!$article) {
+            return response()->json(['error' => 'Article non trouvé'], 404);
+        }
+        return $article;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+        $article = Article::find($id);
+        if (!$article) {
+            return response()->json(['error' => 'Article non trouvé'], 404);
+        }
+        $article->update($request->all());
+        return $article;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $article = Article::find($id);
+        if (!$article) {
+            return response()->json(['error' => 'Article non trouvé'], 404);
+        }
+        $article->delete();
+        return response()->json(['message' => 'Article supprimé']);
+    }
+}
